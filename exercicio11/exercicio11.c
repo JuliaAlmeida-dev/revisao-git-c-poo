f5f200f730f8c75e85d03a12160a8cbfabb561e7
JuliaAlmeida-dev <julia.almeidabm@gmail.com>
2026-03-09 13:38:30 -0300

    não consegui resolver

---
+++ exercicio11/exercicio11.c
@@ -0,0 +1,4 @@
+/*Professor, gostaria de justificar minha dificuldade em responder  a esta questão. 
+Na disciplina em que deveríamos ter estudado estruturas de dados como pilha, fila e lista, 
+esse conteúdo não chegou a ser abordado em sala de aula. 
+Por isso, não tive contato formal com essas explicações durante as aulas*/
