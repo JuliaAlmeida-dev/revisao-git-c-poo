be2b78bc6a797ea66125f2feb5d104b06117db16
JuliaAlmeida-dev <julia.almeidabm@gmail.com>
2026-03-09 13:08:32 -0300

    exercicio 5 resolvido

---
+++ exercicio5/exercicio5.c
@@ -0,0 +1,12 @@
+/*O que é um vetor? Vetor é uma estrutura de dados que permite armazenar varios valores do mesmo  
+tipo em uma variavel só.
+
+a) No C, os elementos de um vetor são acessados por meio de índices que começam em 0, para declarar um vetor é
+necessário informar o tipo dos dados e o tamanho, por exemplo: int numeros[10], cada posição pode ser acessada 
+ou modificada usando seu índice, como numeros[0], numeros[1] e assim por diante.
+
+b) Uma string é uma sequência de caracteres usada para representar textos, em  linguagem C, strings são tratadas
+ como vetores do tipo char em que cada caractere ocupa uma posição do vetor e a sequência termina com um caractere especial
+chamado '\0', que indica o final da string e é esse caractere que permite que o programa saiba onde a string termina.
+
+*/
